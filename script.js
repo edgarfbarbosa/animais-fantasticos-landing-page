f991e249e8974d1a37275422fd61d3f0e2958a5d
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2023-10-06 17:27:58 -0300

    feat: Verificação do conteúdo e ativação do código através de condicional

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 const animaisLista = document.querySelectorAll('.animais-lista li')
 const animaisDescricao = document.querySelectorAll('.animais-descricao section')
 
-/**
- * A função ativarAba adiciona a classe 'ativo' em um index de animaisDescricao.
- * @param {number} index posição no array de animaisDescricao.
- */
-function ativarAba(index) {
-  animaisDescricao.forEach((section) => {
-    section.classList.remove('ativo')
+/** Apenas se animaisLista.length e animaisDescricao.length retornarem true o código irá funcionar. */
+if (animaisLista.length && animaisDescricao.length) {
+  animaisDescricao[0].classList.add('ativo')
+  /**
+  * A função ativarAba adiciona a classe 'ativo' em um index de animaisDescricao.
+  * @param {number} index posição no array de animaisDescricao.
+  */
+  function ativarAba(index) {
+    animaisDescricao.forEach((section) => {
+      section.classList.remove('ativo')
+    })
+    
+    animaisDescricao[index].classList.add('ativo')
+  }
+  
+  animaisLista.forEach((itemMenu, index) => {
+    itemMenu.addEventListener('click', () => {
+      ativarAba(index)
+    })
   })
-
-  animaisDescricao[index].classList.add('ativo')
 }
-
-animaisLista.forEach((itemMenu, index) => {
-  itemMenu.addEventListener('click', () => {
-    ativarAba(index)
-  })
-})

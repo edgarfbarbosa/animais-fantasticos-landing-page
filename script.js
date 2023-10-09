4394ef5fb0c2ad4e1a63c3041db44a7610ff9abd
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2023-10-09 10:17:12 -0300

    fix(activeAccordion): Adição do toggle ao invés de add. Código isolado.

--- script.js
+++ script.js
@@ -27,16 +27,27 @@ function initTabNav() {
 
 initTabNav()
 
-const accordionList = document.querySelectorAll('.faq-lista dt')
-
-function activeAccordion() {
-  // e.preventDefault()
-  // e.currentTarget.nextElementSibling.classList.add('ativo')
-  // Ao invés de utilizar event, eu posso utilizar this que fará referência ao item já selecionado
-  this.classList.add('ativo')
-  this.nextElementSibling.classList.add('ativo')
+function initAccordion() {
+  const accordionList = document.querySelectorAll('.faq-lista dt')
+  
+  if(accordionList.length) {
+    accordionList[0].classList.add('ativo')
+    accordionList[0].nextElementSibling.classList.add('ativo')
+    /**
+     * A função activeAccordion adiciona ou remove a classe ativo dos elementos selecionados na variável accordionList.
+     */
+    function activeAccordion() {
+      // e.preventDefault()
+      // e.currentTarget.nextElementSibling.classList.add('ativo')
+      // Ao invés de utilizar event, eu posso utilizar this que fará referência ao item já selecionado
+      this.classList.toggle('ativo')
+      this.nextElementSibling.classList.toggle('ativo')
+    }
+    
+    accordionList.forEach((item) => {
+      item.addEventListener('click', activeAccordion)
+    })
+  }
 }
 
-accordionList.forEach((item) => {
-  item.addEventListener('click', activeAccordion)
-})
+initAccordion()

39d7caf0dade2e2a9352759b460ef2b178abc215
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2023-10-09 09:36:58 -0300

    fix: Isolado o código para navegação por Tab e alterado o nome da função e variáveis

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
-const animaisLista = document.querySelectorAll('.animais-lista li')
-const animaisDescricao = document.querySelectorAll('.animais-descricao section')
-
-/** Apenas se animaisLista.length e animaisDescricao.length retornarem true o código irá funcionar. */
-if (animaisLista.length && animaisDescricao.length) {
-  animaisDescricao[0].classList.add('ativo')
-  /**
-  * A função ativarAba adiciona a classe 'ativo' em um index de animaisDescricao.
-  * @param {number} index posição no array de animaisDescricao.
-  */
-  function ativarAba(index) {
-    animaisDescricao.forEach((section) => {
-      section.classList.remove('ativo')
-    })
-    
-    animaisDescricao[index].classList.add('ativo')
-  }
+function initTabNav() {
+  const tabMenu = document.querySelectorAll('.animais-lista li')
+  const tabContent = document.querySelectorAll('.animais-descricao section')
   
-  animaisLista.forEach((itemMenu, index) => {
-    itemMenu.addEventListener('click', () => {
-      ativarAba(index)
+  /** Apenas se tabMenu.length e tabContent.length retornarem true o código irá funcionar. */
+  if (tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add('ativo')
+    /**
+    * A função ativarAba adiciona a classe 'ativo' em um index de tabContent.
+    * @param {number} index posição no array de tabContent.
+    */
+    function ativarAba(index) {
+      tabContent.forEach((section) => {
+        section.classList.remove('ativo')
+      })
+      
+      tabContent[index].classList.add('ativo')
+    }
+    
+    tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener('click', () => {
+        ativarAba(index)
+      })
     })
-  })
+  }
 }
+
+initTabNav()

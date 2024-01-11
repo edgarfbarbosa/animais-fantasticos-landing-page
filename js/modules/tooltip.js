49c2ea34b25354f4d6b883c751b8927ab6725983
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2024-01-11 19:01:38 -0300

    feat(tooltip): Adiciona JSDoc para melhorar a legibilidade do código

--- js/modules/tooltip.js
+++ js/modules/tooltip.js
@@ -1,10 +1,22 @@
+/**
+* Inicializa funcionalidade tooltip em todos os elementos que possuem o atributo data-tooltip.
+* Quando o mouse passa sobre um desses elementos, um tooltip é criado e exibido.
+* A posição do tooltip segue o movimento do mouse.
+* @function
+*/
 export default function initTooltip() {
+  /** Seleciona todos os elementos com o atributo data-tooltip */
   const tooltips = document.querySelectorAll('[data-tooltip]')
-  
+  /** Adiciona o evento 'mouseover' em cada elemento selecionado */
   tooltips.forEach((item) => {
     item.addEventListener('mouseover', onMouseOver)
   })
   
+  /**
+  * Função chamada quando o evento 'mouseover' é disparado.
+  * A função cria uma caixa de informação para o elemento e adiciona event listeners para 'mousemove' e 'mouseleave'.
+  * @param {*} e - Evento do mouse. 
+  */
   function onMouseOver(e) {
     const tooltipBox = criarTooltipBox(this)
     
@@ -16,6 +28,11 @@ export default function initTooltip() {
     this.addEventListener('mouseleave', onMouseLeave)  
   }
   
+  /**
+  * Objeto que lida com o evento 'mouseleave'.
+  * Remove o tooltip e os event listeners associados ao elemento.
+  * Esse objeto tem um método 'handleEvent' que é chamado quando o evento é disparado.
+  */
   const onMouseLeave = {
     handleEvent() {
       this.tooltipBox.remove()
@@ -24,6 +41,11 @@ export default function initTooltip() {
     }
   }
   
+  /**
+  * Objeto que lida com o evento 'mousemove'.
+  * Atualiza posição do tooltip para seguir o movimento do mouse.
+  * Esse objeto tem um método 'handleEvent' que é chamado quando o evento é disparado.
+  */
   const onMouseMove = {
     handleEvent(e) {
       this.tooltipBox.style.top = e.pageY + 20 + 'px'
@@ -31,6 +53,12 @@ export default function initTooltip() {
     }
   }
   
+  /**
+  * Cria um tooltip para o elemento especificado.
+  * O tooltip contém o texto do atributo aria-label do elemento.
+  * @param {HTMLElement} element - Elemento para qual o tooltip deve ser criado. 
+  * @returns {HTMLElement} Tooltip criado.
+  */
   function criarTooltipBox(element) {
     const tooltipBox = document.createElement('div')
     const text = element.getAttribute('aria-label')

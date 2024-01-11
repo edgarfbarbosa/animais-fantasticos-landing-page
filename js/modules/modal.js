dded6754023860ad40935f1e1b2ee30290ea50c0
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2024-01-11 19:09:09 -0300

    feat(modal): Adiciona JSDoc para melhorar legibilidade do código

--- js/modules/modal.js
+++ js/modules/modal.js
@@ -1,3 +1,9 @@
+/**
+* Implementa o modal do projeto, sua manipulação ocorre através de cliques nos butões do modal ou fora da área do modal.
+* A função seleciona todos os elementos especificados com o atributo data-modal.
+* A função adiciona ou remove a classe 'ativo' ao elemento containerModal.
+* @returns {HTMLElement} Retorna o container do modal.
+*/
 export default function initModal() {
   const botaoAbrir = document.querySelector('[data-modal="abrir"]')
   const botaoFechar = document.querySelector('[data-modal="fechar"]')

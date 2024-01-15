484ff375f1c2cca3359cfaaafef143c4cac3b998
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2024-01-15 20:54:19 -0300

    feat(dropdown-menu): adiciona funcionalidade de fechar submenu ao clicar fora dele

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -11,9 +11,28 @@ dropdownMenus.forEach( menu => {
 /**
 * A função handleClick adiciona a classe 'active' ao submenu clicado.
 * O 'this' se refere ao elemento atribuido no event listener que chama essa função.
+* A função ativa a função outsideClick.
 * @param {*} event - Evento que aciona a função.
 */
 function handleClick(event) {
   event.preventDefault()
-  this.classList.toggle('active')
+  this.classList.add('active')
+  outsideClick(this, () => {
+    this.classList.remove('active')
+  })
+}
+
+/**
+* A função ousideClick seleciona o html e adiciona evento a ele.
+*/
+function outsideClick(element, callback) {
+  const html = document.documentElement
+  html.addEventListener('click', handleOutsideClick)
+  
+  function handleOutsideClick(event) {
+    if (!element.contains(event.target )) {
+      html.removeEventListener('click', handleOutsideClick)
+      callback()
+    }
+  }
 }

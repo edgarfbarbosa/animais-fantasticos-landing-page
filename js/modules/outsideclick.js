767f1e9326032de4745f79f446aab65e96b37423
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2024-01-16 18:17:17 -0300

    feat(outsideclick): adiciona função outsideClick

---
+++ js/modules/outsideclick.js
@@ -0,0 +1,41 @@
+/**
+* A função ousideClick seleciona o html e adiciona evento a ele.
+* A função foi adicionado em um arquivo próprio para reutilização. Caso seja necessário utilizar a mesma em um menu mobile.
+* @param {HTMLElement} element - O elemento HTML que deve ser monitorado para cliques fora dele.
+* @param {Array<string>} events - Lista de nome de eventos que devem ser monitorados.
+* @param {Function} callback - A função que deve ser chamada quando um clique fora do elemento é detectado.
+*/
+export default function outsideClick(element, events, callback) {
+  // Seleciona o elemento raiz do documento HTML
+  const html = document.documentElement
+  // Atributo personalizado para marcar elementos que foram configurados com este manipulador de eventos.
+  const outside = 'data-outside'
+  
+  /** 
+  * Somente se o elemento não tem 'data-outside' o event listener é adicionado. Após adição do atributo 'data-outside', o event listener não é adicionado novamente.
+  * Previne a adição de diversos Event Listeners no navegador. 
+  */
+  if (!element.hasAttribute(outside)) {
+    events.forEach(userEvent => {
+      // Adiciona os listeners dos eventos ao elemento HTML
+      html.addEventListener(userEvent, handleOutsideClick)
+    })
+    // Marca o elemento como configurado
+    element.setAttribute(outside, '')
+  }
+  
+  /**
+  * Função interna que lida com os eventos de clique fora do elemento.
+  * Se o clique aconteceu fora do elemento, remove o atributo personalizado e chama a função de retorno de chamada.
+  * @param {MouseEvent} event - O evento do mouse que disparou a chamada. 
+  */
+  function handleOutsideClick(event) {
+    if (!element.contains(event.target )) {
+      element.removeAttribute(outside)
+      events.forEach(userEvent => {
+        html.addEventListener(userEvent, handleOutsideClick)
+      })
+      callback()
+    }
+  }
+}

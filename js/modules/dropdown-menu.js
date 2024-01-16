e846b15280a64ea82245db0d13f85685cbd85b6e
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2024-01-16 18:18:19 -0300

    feat(dropdown-menu): refatora função initDropdownMenu

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -1,38 +1,30 @@
-export default function initDropdownMenu() {}
-
-/** Seleciona todos os submenus */
-const dropdownMenus = document.querySelectorAll('[data-dropdown]')
-
-/** Utiliza o forEach para adicionar o event listener 'click' e 'touchstart' a cada submenu. */
-dropdownMenus.forEach( menu => {
-  ['touchstart', 'click'].forEach(userEvent => menu.addEventListener(userEvent, handleClick))
-})
+import outsideClick from './outsideclick.js'
 
 /**
-* A função handleClick adiciona a classe 'active' ao submenu clicado.
-* O 'this' se refere ao elemento atribuido no event listener que chama essa função.
-* A função ativa a função outsideClick.
-* @param {*} event - Evento que aciona a função.
+* A função 'initDropdownMenu' inicializa o menu suspenso.
+* Ela seleciona todos os elementos suspensos na página e adiciona um event listener para os eventos 'touchstart' e 'click'.
+* Quando um desses eventos é disparado em um menu suspenso, a função 'handleClick' é chamada.
 */
-function handleClick(event) {
-  event.preventDefault()
-  this.classList.add('active')
-  outsideClick(this, () => {
-    this.classList.remove('active')
-  })
-}
-
-/**
-* A função ousideClick seleciona o html e adiciona evento a ele.
-*/
-function outsideClick(element, callback) {
-  const html = document.documentElement
-  html.addEventListener('click', handleOutsideClick)
+export default function initDropdownMenu() {
+  // Seleciona todos os submenus
+  const dropdownMenus = document.querySelectorAll('[data-dropdown]')
   
-  function handleOutsideClick(event) {
-    if (!element.contains(event.target )) {
-      html.removeEventListener('click', handleOutsideClick)
-      callback()
-    }
+  // Utiliza o forEach para adicionar o event listener 'click' e 'touchstart' a cada submenu.
+  dropdownMenus.forEach( menu => {
+    ['touchstart', 'click'].forEach(userEvent => menu.addEventListener(userEvent, handleClick))
+  })
+  
+  /**
+  * A função handleClick adiciona a classe 'active' ao submenu clicado.
+  * O 'this' se refere ao elemento atribuido no event listener que chama essa função.
+  * A função ativa a função outsideClick.
+  * @param {*} event - Evento que aciona a função.
+  */
+  function handleClick(event) {
+    event.preventDefault()
+    this.classList.add('active')
+    outsideClick(this, ['touchstart', 'click'], () => {
+      this.classList.remove('active')
+    })
   }
 }
